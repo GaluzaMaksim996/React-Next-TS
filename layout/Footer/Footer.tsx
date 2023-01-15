63c3ff16dc8e448df354672d747389a2f019361a
Maksim Galuza <krakket@bk.ru>
2023-01-15 20:00:27 +0300

    added markup footer

---
+++ layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { FooterProps } from './Footer.props';
+
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer>
+      <div>footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
