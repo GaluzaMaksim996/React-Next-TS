f1fd4fbbc7442bca66b27efbb195b6a19603bb70
Maksim Galuza <krakket@bk.ru>
2023-01-15 20:01:55 +0300

    added Sidebar

---
+++ layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import { SidebarProps } from './Sidebar.props';
+import styles from './Sidebar.module.css';
+import cn from 'classnames';
+import Logo from '../logo.svg';
+
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
+  return (
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+    </div>
+  );
+};
