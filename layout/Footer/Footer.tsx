476fa91a66e0fe5508f5464cee2d75c69ba82109
Maksim Galuza <krakket@bk.ru>
2023-01-15 21:07:58 +0300

    added footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,17 @@
 import { FooterProps } from './Footer.props';
+import styles from './Footer.module.css';
+import cn from 'classnames';
 
 const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
   return (
-    <footer>
-      <div>footer</div>
+    <footer className={cn(className, styles.footer)}>
+      <div>OwlTop © 2023 - Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
     </footer>
   );
 };
