32e694a60a1bca528c1d384f812d059014bc3cd1
Maksim Galuza <krakket@bk.ru>
2023-01-17 20:25:44 +0300

    added component Input

---
+++ components/Input/Input.tsx
@@ -0,0 +1,12 @@
+import { InputProps } from './Input.props';
+import styles from './Input.module.css';
+import { ForwardedRef, forwardRef } from 'react';
+import cn from 'classnames';
+
+export const Input = forwardRef(({ className, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
+  return (
+    <div className={cn(className, styles.inputWrapper)}>
+      <input className={styles.input} {...props} />
+    </div>
+  );
+});
