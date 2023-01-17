57692a18fe95311ea9bb1757ff3ee022bbc142c9
Maksim Galuza <krakket@bk.ru>
2023-01-17 20:48:13 +0300

    added component Textarea

---
+++ components/Textarea/Textarea.tsx
@@ -0,0 +1,12 @@
+import { TextareaProps } from './Textarea.props';
+import styles from './Textarea.module.css';
+import cn from 'classnames';
+import { ForwardedRef, forwardRef } from 'react';
+
+export const Textarea = ({ className, ...props }: TextareaProps, ref: ForwardedRef<HTMLTextAreaElement>): JSX.Element => {
+  return (
+    <div className={cn(styles.textareaWrapper, className)}>
+      <textarea className={styles.textarea} {...props} />
+    </div>
+  );
+};
